0fff084d60702eb8361a2b1c2c57ea13bcc8901a
AnthonySchoolWctc <Adzikowski@my.wctc.edu>
2025-12-09 18:03:23 -0600

    commit 8

--- Program.cs
+++ Program.cs
@@ -1,11 +1,37 @@
 ﻿using NLog;
+
+
+using System.Linq;
+using NorthwindConsole.Model;
+
 string path = Directory.GetCurrentDirectory() + "//nlog.config";
 
 // create instance of Logger
 var logger = LogManager.Setup().LoadConfigurationFromFile(path).GetCurrentClassLogger();
 
 logger.Info("Program started");
+do
+{
+  Console.WriteLine("1) Display categories");
+  Console.WriteLine("2) Add category");
+  Console.WriteLine("Enter to quit");
+  string? choice = Console.ReadLine();
+  Console.Clear();
+  logger.Info("Option {choice} selected", choice);
 
-Console.WriteLine("Hello World!");
+  if (choice == "1")
+  {
+    // display categories
+  }
+  else if (choice == "2")
+  {
+    // Add category
+  }
+  else if (String.IsNullOrEmpty(choice))
+  {
+    break;
+  }
+  Console.WriteLine();
+} while (true);
 
 logger.Info("Program ended");
